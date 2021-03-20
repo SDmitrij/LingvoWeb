5783b368b254fb4a94f4a59f77bc360363d44d91
Щербак Александр Николаевич <sawa_96@bk.ru>
2021-03-20 21:18:39 +0300

    request validation

--- Controllers/TranslateController.cs
+++ Controllers/TranslateController.cs
@@ -1,5 +1,4 @@
-﻿using LingvoWeb.Models;
-using LingvoWeb.Service;
+﻿using LingvoWeb.Service;
 using LingvoWeb.Service.Json;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -17,11 +16,24 @@ namespace LingvoWeb.Controllers
         }
 
         [HttpGet("get")]
-        public async Task<JsonResult> GetTranslation(string text, string srcLang, string dstLang, bool isShort)
+        public async Task<JsonResult> GetTranslation(string text, string srcLang, string dstLang, bool isShort = true)
         {
+            Console.WriteLine("Translate request {0} from {1} to {2}", text, srcLang, dstLang);
+
+            if(!ValidateTranslationRequest(text, srcLang, dstLang))
+            {
+                Console.WriteLine("There is empty or missing parameters.");
+                var res = new JsonResult("There is empty or missing parameters.")
+                {
+                    StatusCode = 400
+                };
+                return res;
+            }
+
             if (!Enum.TryParse(srcLang, out LanguageEnum srcLangEnum)
             || !Enum.TryParse(dstLang, out LanguageEnum dstLangEnum))
             {
+                Console.WriteLine("Wrong language.");
                 var res = new JsonResult("Can't find required language.")
                 {
                     StatusCode = 400
@@ -32,7 +44,14 @@ namespace LingvoWeb.Controllers
                 await Translator.GetTranslation(text, srcLangEnum, dstLangEnum, isShort);
 
             var transRes = JsonSerializer.Deserialize<JsonShortResult>(json).Translation.Translation;
+
             return new JsonResult(transRes);
+        }
+        private bool ValidateTranslationRequest (string text, string srcLang, string dstLang)
+        {
+            return !string.IsNullOrWhiteSpace(text) &&
+                !string.IsNullOrWhiteSpace(srcLang) &&
+                !string.IsNullOrWhiteSpace(dstLang);
         }
     }
 }
